7ba401ee4e9f0caf02a765807320ac47185ca320
Felipe Geremia-Nievinski <fgnievinski@gmail.com>
2022-02-07 12:54:38 -0300

    Add files via upload

---
+++ setup_altim_interf.m
@@ -0,0 +1,13 @@
+function setup_altim_interf ()
+    persistent has_been_ran
+    if isempty(has_been_ran),  has_been_ran = false;  end
+    if has_been_ran,  return;  end
+    %addpath(genpath(pwd()))
+    dir = pwd();
+    if strcmpi(dir(end-3:end), 'demo')
+      dir = fullfile(dir, '..');  % go up one level.
+    end
+    addpath(genpath(dir))
+    has_been_ran = true;
+end
+
