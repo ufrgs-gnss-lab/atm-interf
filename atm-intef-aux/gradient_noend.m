6ed4b1b750fb625f24e098f4aa9a3a6c1787275a
fgnievinski <fgnievinski@gmail.com>
2022-06-10 21:01:38 -0300

    renaming subfolder

--- atm-intef-aux/gradient_noend.m
+++
@@ -1,9 +0,0 @@
-%GRADIENT_NOEND Gradient with disabled end-points (where it's inaccurate).
-function c = gradient_noend (varargin)
-    c = gradient (varargin{:});
-    c(1) = NaN;
-    c(end) = NaN;
-end
-
-% gradient_noend = @(varargin) setel(setel(gradient(varargin{:}), 1,NaN), 'end',NaN);
-
---
+++ atm-interf-aux/get_height_from_delay.m
@@ -0,0 +1,28 @@
+function height = get_height_from_delay (delay, elev, dim, noend)
+%GET_HEIGHT_FROM_DELAY: Get equivalent reflector height.
+% Input:
+%   delay [meters]
+%   elev [degrees]
+% Output:
+%   height_equiv [meters]
+%   vertwavenum [degrees per meter]
+% See also: get_height_from_phase
+
+  if (nargin < 3),  dim = [];  end
+  if (nargin < 4),  noend = [];  end
+  if isvector(elev)
+      height = gradient_all(delay, 2*sind(elev), [], noend);
+      return;
+  end
+  if isempty(dim)
+      if is_uniform(elev(:,2))
+          dim = 2;
+      elseif is_uniform(elev(:,1))
+          dim = 1;
+      else
+          error('SNR:get_height_from_delay:badInp', ...
+              'Unknown elevation variation dimension.');
+      end
+  end
+  height = gradient_all(delay, 2*sind(elev), dim, noend);
+end
